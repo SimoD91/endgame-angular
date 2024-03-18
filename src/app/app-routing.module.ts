879b7e2e80424c4cf62d1ac8b8ba614e6015a810
SimoD91 <simone0091@hotmail.com>
2024-03-18 22:49:24 +0100

    initialize endgame front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'user-page', loadChildren: () => import('./pages/user-page/user-page.module').then(m => m.UserPageModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
