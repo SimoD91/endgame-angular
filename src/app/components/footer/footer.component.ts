a20222c63d4334dec1f59c753059ca058cfd80df
SimoD91 <simone0091@hotmail.com>
2024-03-27 06:31:13 +0100

    delete useless function

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -11,25 +11,4 @@ hideFooter: boolean = false;
 
 constructor(private router: Router){
 }
-/*
-//--- Metodo per rimuovere footer durante lo scroll della Portal page ---\\
-ngOnInit(): void {
-  if (typeof window !== 'undefined') {
-    this.controlSizePage();
-    window.addEventListener('resize', () => {
-      this.controlSizePage();
-    });
-  }
-}
-
-controlSizePage(){
-  const belowidth = 991.50;
-  const ispage = this.router.url === '/dashboard/portal' || this.router.url === '/dashboard/videogame';
-  if(window.innerWidth < belowidth && ispage){
-    this.hideFooter = true;
-  }else{
-    this.hideFooter = false;
-  }
-}
-*/
 }
